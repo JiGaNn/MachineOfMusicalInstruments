965b3b1dcc2791820c50282ffe43d4f22fae4b8d
Евгений Афанасьев <evgenij29082002@gmail.com>
2021-11-14 20:01:16 +0800

    Добавьте файлы проекта.

---
+++ MachineOfMusicalInstruments/MusicalInstruments.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineOfMusicalInstruments
+{
+    public enum MusInstType { acoustic, electronic }
+    abstract public class MusInst
+    {
+        public MusInstType type;
+    }
+    public class Guitar : MusInst
+    {
+        public int amountOfString;
+        public string builds;
+    }
+    public class Piano : MusInst
+    {
+        public int amountOfKeys;
+        public int amountOfFullOctaves;
+    }
+    public enum DrumType { big, small, timpani, tom }
+    public class Drum : MusInst
+    {
+        public int radius;
+        public DrumType typeOfDrum;
+    }
+}
